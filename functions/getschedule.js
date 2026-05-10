ca305d3320003e8f98e5520a836e5e33088991dc
Alex Duan <alexduan@Alexs-MacBook-Air-10.local>
2026-05-10 17:04:33 -0400

    minor tweaks

--- functions/getschedule.js
+++ functions/getschedule.js
@@ -2,7 +2,7 @@ const headers = {
     'content-type' : 'application/json',
     'Access-Control-Allow-Origin': '*'
 };
-const years = [2023, 2024, 2025];
+const years = [2023, 2024, 2025, 2026];
 exports.handler = async (event) => {
     if(event.queryStringParameters.year==2025){
         data = require('./schedule2025.json');
@@ -12,6 +12,9 @@ exports.handler = async (event) => {
     }
     else if(event.queryStringParameters.year==2023){
         data = require('./schedule2023.json');
+    }
+    else if(event.queryStringParameters.year==2026){
+        data = require('./schedule2026.json');
     }
     return {
         body: JSON.stringify(data),
